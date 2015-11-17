ae668b5dc72e7f1ccbb57ef32fdf2495dbcbb703
jason-ziolo <jziolo@mymail.mines.edu>
2015-11-17 11:46:27 -0700

    removed unnecessary variables, and method within ComputerPlayer

--- src/clueGame/HumanPlayer.java
+++ src/clueGame/HumanPlayer.java
@@ -1,9 +1,6 @@
 package clueGame;
 
 public class HumanPlayer extends Player {
-	private int pcol;
-	private int prow;
-	
 	public HumanPlayer(String playerName, int row, int column, String color) {
 		super(playerName, row, column, color);
 	}
