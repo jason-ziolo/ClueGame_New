bda99938241b07502698c476f226527648cc36f8
jason-ziolo <jziolo@mymail.mines.edu>
2015-11-22 16:54:58 -0700

    adding accusation dlg option

--- src/clueGUI/MakeGuessDialog.java
+++ src/clueGUI/MakeGuessDialog.java
@@ -16,18 +16,17 @@ import clueGame.ClueGame;
 import clueGame.Solution;
 
 public class MakeGuessDialog extends JDialog {
-	// NAME is both the name of the window and the title
-	static public final String NAME = "Make a Guess";
 	// Expected dimensions of 300 x 200.
 	static public final int WIDTH = 300;
 	static public final int HEIGHT = 200;
+	private boolean isAccusationMenu;
 	private JButton submitBtn;
 	private JButton cancelBtn;
 	private JComboBox<String> personPopup;
 	private JComboBox<String> weaponPopup;
 	private JTextField yourRoom = new JTextField("");
 	
-	public MakeGuessDialog(ArrayList<String> people, ArrayList<String> weapons) {
+	public MakeGuessDialog(ArrayList<String> people, ArrayList<String> weapons, boolean isAccusationMenu) {
 		/* MakeGuessDialog will use grid layout, with 2 columns and 4 rows
 		 * There will be, in order of addition:
 		 * 3 text fields (non-editable)
@@ -37,8 +36,10 @@ public class MakeGuessDialog extends JDialog {
 		 * A submit button, which will call ClueGame.playerSuggestion
 		 * A cancel button, which will close the dialog (set it non-visible)
 		 */
-		this.setName(NAME);
-		this.setTitle(NAME);
+		//if()
+		String name = "";
+		this.setName(name);
+		this.setTitle(name);
 		this.setSize(WIDTH, HEIGHT);
 		this.setLayout(new GridLayout(4, 2));
 		// first row: "Your room" label, then static text field
